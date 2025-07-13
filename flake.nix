30aa19f0f8623c645a410751663e7b98a4c9323c
Kierán Meinhardt <kmein@posteo.de>
2025-07-13 13:05:56 +0200

    make another awesome graph

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       hash = "sha256-ycR59YV/9GHLy58PD2kveY+F1bnARiqTCswIN3NmjVI=";
     };
 
-    system = "x86_64-darwin";
+    system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     lib = nixpkgs.lib;
     zip-font = name: arguments: let
